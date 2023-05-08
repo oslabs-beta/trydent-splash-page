2bbc4817bba1b1ae1a4ea1ae390527d5d506206b
Nicholas <lynick14@gmail.com>
2023-05-08 18:37:16 -0400

    create introsection and style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
 import '../index.css'
+import IntroSection from './IntroSection'
 import TeamSection from './TeamSection'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
-      <article>
-        <TeamSection></TeamSection>
-      </article>
+      <IntroSection></IntroSection>
+      <TeamSection></TeamSection>
     </>
     
   )
