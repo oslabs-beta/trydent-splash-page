83314a043ad6cba7e6080c68f441ac696e329b66
Nicholas <lynick14@gmail.com>
2023-05-11 15:49:13 -0400

    update blur on image popup

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -25,7 +25,7 @@ const IntroSection:React.FC = () =>{
           <a className="inline-block bg-gray-300 hover:bg-blue-600 text-blue-900 hover:text-gray-300 font-bold py-2 px-4 rounded-full transition-colors duration-300" href="https://trydent.mintlify.app/introduction">Get Started</a>
         </article>
         <div
-          className={` ${isClicked ? ' flex justify-center items-center min-h-screen fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-50' : ''}`}
+          className={` ${isClicked ? ' flex justify-center items-center min-h-screen fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-20 backdrop-blur-sm  z-50' : ''}`}
           onClick={handleClick}
         >
           <img src={gif}
